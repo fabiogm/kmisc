f438b1cdb5c1bd9a0b3e820ee1914b5c2fd5a840
fabiogm <fmagalhaes@gmail.com>
2015-07-18 22:10:37 -0300

    Adding more filtering functions and diagnostic plots

--- kmisc.R
+++ kmisc.R
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+library(car)
+
 # readObject
 #
 # (file, ...) -> data.frame
@@ -48,7 +50,6 @@ dropColumns <- function(obj, cols) {
     obj[, !(colnames(cols) %in% cols)]
 }
 
-
 # selectColumns
 #
 # (data.frame, c() of strings) -> data.frame 
@@ -58,43 +59,52 @@ selectColumns <- function(obj, cols) {
     obj[, colnames(cols) %in% cols]
 }
 
-getTrainSplit <- function(train, label, holdout.percentage=0.2, drop.labels=T) {
+#getTrainSplit <- function(train, label, holdout.percentage=0.2, drop.labels=T) {
+#
+#    if (holdout.percentage <= 0 || holdout.percentage >= 1) {
+#        stop('Holdout percentage outside (0,1) interval.')
+#    }
+#
+#    n <- nrow(train)
+#    offset <- ceiling(n*(1 - holdout.percentage))
+#    df <- train[sample(n, n),]
+#    train_split <- df[1:offset,]
+#    test_split <- df[(offset+1):n,]
+#    y <- test_split[[label]]
+#
+#    if (drop.labels) {
+#        test_split[[label]] <- NULL
+#    }
+#
+#    return(list(train=train_split, test=test_split, y=y))
+#}
 
+createTrainSplit <- function(train, holdout.percentage=0.2) {
     if (holdout.percentage <= 0 || holdout.percentage >= 1) {
         stop('Holdout percentage outside (0,1) interval.')
     }
 
     n <- nrow(train)
-    offset <- ceiling(n*(1 - holdout.percentage))
-    df <- train[sample(n, n),]
-    train_split <- df[1:offset,]
-    test_split <- df[(offset+1):n,]
-    y <- test_split[[label]]
-
-    if (drop.labels) {
-        test_split[[label]] <- NULL
-    }
-
-    return(list(train=train_split, test=test_split, y=y))
-}
-
-createTrainSplit <- function(train, holdout.percentage=0.2) {
-    n <- nrow(train)
     offset <- ceiling(n * (1 - holdout.percentage))
     return(sample(n, offset))
 }
 
-createFormula <- function(object, target, todrop) {
+createFormula <- function(object, target, todrop=NULL) {
     todrop <- c(todrop, target)
     cols <- colnames(object)[!(colnames(object) %in% todrop)]
     return(as.formula(paste(target, '~', paste(cols, collapse='+'))))
 }
 
-createDummyVariables <- function(dataf, vars, full.rank=T) {
+# createDummyVariables
+#
+# (data.frame, c(), boolean) -> data.frame
+#
+# Creates a data.frame with additional dummy variables for the selected variables.
+createDummyVariables <- function(dataf, vars, full.rank=T, sep='_') {
     if (full.rank) {
         for (vr in vars) {
             for (level in unique(dataf[[vr]])) {
-                dataf[paste("dummy", gsub(" ", "", level), sep="_")] <- ifelse(dataf[[vr]] == level, 1, 0)
+                dataf[paste("dummy", gsub(" ", "", level), sep=sep)] <- ifelse(dataf[[vr]] == level, 1, 0)
             }
         }
         return(dataf)
@@ -102,6 +112,76 @@ createDummyVariables <- function(dataf, vars, full.rank=T) {
         dummies <- model.matrix(as.formula(paste('~', paste(vars, collapse='+'))), data=dataf)[, -1]
         return(cbind(dataf, dummies))
     }
+}
+
+filterByClass <- function(dataf, cl) {
+    cols <- as.vector(sapply(dataf, function(x) class(x) == cl))
+    return(dataf[, cols])
+}
+
+filterNumeric <- function(dataf) {
+    cols <- as.vector(sapply(dataf, is.numeric))
+    return(dataf[, cols])
+}
+
+paster <- function(...) {
+    paste(..., collapse='')
+}
+
+#
+# Heuristic plots
+#
+
+# Inspired by 'Data Mining with R' by Luís Torgo
+normalityPlot <- function(xs, prob=F, xlab='', main='', showDensity=T, showJitter=T) {
+    expr <- deparse(substitute(xs))
+    par(mfrow=c(1,2))
+
+    if (main == '') {
+        main <- paste('Histogram of', expr)
+    }
+
+    if (xlab == '') {
+        xlab <- expr
+    }
+
+    hist(xs, prob=prob, xlab=xlab, main=main)
+
+    if (showDensity) {
+        lines(density(xs, na.rm=T))
+    }
+
+    if (showJitter) {
+        rug(jitter(xs))
+    }
+
+    qqPlot(xs, main=paste('Normal QQ plot of', expr))
+    par(mfrow=c(1,1))
+}
+
+boxPlot <- function(xs, ylab='', showJitter=T, showMean=T) {
+    if (ylab == '') {
+        ylab <- deparse(substitute(xs))
+    }
+
+    boxplot(xs, ylab=ylab)
+
+    if (showJitter) {
+        rug(jitter(xs), side=2)
+    }
+
+    if (showMean) {
+        abline(h = mean(xs, na.rm=T), lty=2)
+    }
+}
+
+normHist <- function(data) {
+    h <- hist(data)
+    multiplier <- h$counts/h$density
+    dens <- density(data)
+    dens$y <- dens$y * multiplier[1]
+    plot(h)
+    lines(dens)
 }
 
 #
@@ -114,5 +194,20 @@ LogLoss <- function(act, pred, eps=1e-15) {
     ll <- sum(act*log(pred))
     ll <- ll * (-1/length(act))
     return (ll)
+}
+
+rmse <- function(y, y_hat) {
+    sqrt(mean((y-y_hat)^2))
+}
+
+# From "R in Action" by Robert Kabacoff
+wssplot <- function(data, nc=15, seed=1234) {
+    wss <- (nrow(data)-1)*sum(apply(data,2,var))
+    for (i in 2:nc) {
+        set.seed(seed)
+        wss[i] <- sum(kmeans(data, centers=i)$withinss)
+    }
+    plot(1:nc, wss, type="b", xlab="Number of Clusters",
+         ylab="Within groups sum of squares")
 }
 
